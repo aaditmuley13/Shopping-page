2cdaa624dd021951c423a1f1331558fb59473f9c
aaditmuley13 <muleyaadit@gmail.com>
2024-09-30 22:12:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import"./App.css"
+import Navigation from "./components/Navigation"
+import Mainsection from "./components/Mainsection";
+const App=()=>{
+  return(
+    <div>
+     <Navigation/> 
+      <Mainsection/>
+       
+    </div>
+    
+  );
+};
+export default App;
